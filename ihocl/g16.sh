30ddc8e5757d8145caff22656d18085b31be3b91
C. Gavin-Hanner <cgavinh@n2232.hyak.local>
2021-09-28 09:20:26 -0700

    ran sp's and made cubes for ihocl

--- ihocl/g16.sh
+++ ihocl/g16.sh
@@ -19,17 +19,17 @@ set
 echo "**********************************************" 
 
 # run Gaussian
-#g16 ihocl_0_opt.gjf
+g16 ihocl_0_opt.gjf
 g16 ihocl_0_sp.gjf
-#g16 ihocl_5_sp.gjf
-#g16 ihocl_10_sp.gjf
-#g16 ihocl_15_sp.gjf
-#g16 ihocl_20_sp.gjf
-#g16 ihocl_25_sp.gjf
-#g16 ihocl_30_sp.gjf
-#g16 ihocl_35_sp.gjf
-#g16 ihocl_40_sp.gjf
-#g16 ihocl_45_sp.gjf
-#g16 ihocl_50_sp.gjf
+g16 ihocl_5_sp.gjf
+g16 ihocl_10_sp.gjf
+g16 ihocl_15_sp.gjf
+g16 ihocl_20_sp.gjf
+g16 ihocl_25_sp.gjf
+g16 ihocl_30_sp.gjf
+g16 ihocl_35_sp.gjf
+g16 ihocl_40_sp.gjf
+g16 ihocl_45_sp.gjf
+g16 ihocl_50_sp.gjf
 #g16 ihocl_rOH_opt.gjf
 
