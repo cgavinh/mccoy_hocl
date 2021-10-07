cda3357b06379c8a5184f23fe7eae0cbfd20b2a0
C. Gavin-Hanner <cgavinh@mox1.hyak.local>
2021-10-06 17:39:43 -0700

    clhod

---
+++ oop_s_rOH_opt/clhod/g16.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=clhod
+#SBATCH --nodes=1 
+#SBATCH --time=3:00:00
+#SBATCH --mem=128G
+#SBATCH --chdir=/gscratch/ilahie/mccoygrp/coire_hocl/oop_s_rOH_opt/clhod/
+#SBATCH --partition=ilahie
+#SBATCH --account=ilahie
+
+# load Gaussian environment
+module load contrib/g16.b01
+
+# debugging information
+echo "**** Job Debugging Information ****"
+echo "This job will run on $SLURM_JOB_NODELIST"
+echo ""
+echo "ENVIRONMENT VARIABLES"
+set
+echo "**********************************************" 
+
+# run Gaussian
+#g16 clhod_0_opt.gjf
+g16 clhod_oop_sp.gjf
