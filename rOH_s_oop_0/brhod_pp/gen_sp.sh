1df0a2f88b09dbef8214bf5e8910ba1b7282a747
C. Gavin-Hanner <cgavinh@mox1.hyak.local>
2021-10-15 15:39:26 -0700

    brhod_pp

---
+++ rOH_s_oop_0/brhod_pp/gen_sp.sh
@@ -0,0 +1,17 @@
+#!bin/bash/
+
+sed -e 's\075\080\g' -e 's\0.75\0.80\g' brhod_pp_rOH_075_sp.gjf > brhod_pp_rOH_080_sp.gjf
+sed -e 's\075\085\g' -e 's\0.75\0.85\g' brhod_pp_rOH_075_sp.gjf > brhod_pp_rOH_085_sp.gjf
+sed -e 's\075\090\g' -e 's\0.75\0.90\g' brhod_pp_rOH_075_sp.gjf > brhod_pp_rOH_090_sp.gjf
+sed -e 's\075\095\g' -e 's\0.75\0.95\g' brhod_pp_rOH_075_sp.gjf > brhod_pp_rOH_095_sp.gjf
+sed -e 's\075\100\g' -e 's\0.75\1.00\g' brhod_pp_rOH_075_sp.gjf > brhod_pp_rOH_100_sp.gjf
+sed -e 's\075\105\g' -e 's\0.75\1.05\g' brhod_pp_rOH_075_sp.gjf > brhod_pp_rOH_105_sp.gjf
+sed -e 's\075\110\g' -e 's\0.75\1.10\g' brhod_pp_rOH_075_sp.gjf > brhod_pp_rOH_110_sp.gjf
+sed -e 's\075\115\g' -e 's\0.75\1.15\g' brhod_pp_rOH_075_sp.gjf > brhod_pp_rOH_115_sp.gjf
+sed -e 's\075\120\g' -e 's\0.75\1.20\g' brhod_pp_rOH_075_sp.gjf > brhod_pp_rOH_120_sp.gjf
+sed -e 's\075\125\g' -e 's\0.75\1.25\g' brhod_pp_rOH_075_sp.gjf > brhod_pp_rOH_125_sp.gjf
+sed -e 's\075\130\g' -e 's\0.75\1.30\g' brhod_pp_rOH_075_sp.gjf > brhod_pp_rOH_130_sp.gjf
+sed -e 's\075\135\g' -e 's\0.75\1.35\g' brhod_pp_rOH_075_sp.gjf > brhod_pp_rOH_135_sp.gjf
+sed -e 's\075\140\g' -e 's\0.75\1.40\g' brhod_pp_rOH_075_sp.gjf > brhod_pp_rOH_140_sp.gjf
+sed -e 's\075\145\g' -e 's\0.75\1.45\g' brhod_pp_rOH_075_sp.gjf > brhod_pp_rOH_145_sp.gjf
+sed -e 's\075\150\g' -e 's\0.75\1.50\g' brhod_pp_rOH_075_sp.gjf > brhod_pp_rOH_150_sp.gjf
---
+++ rOH_s_oop_0/ihod/fchk.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+for f in ihod_rOH*.chk
+do
+	echo $f
+done
