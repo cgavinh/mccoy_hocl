c7dc8946a5c3a9a5aa7cc22d14657f6fd9de5903
C. Gavin-Hanner <cgavinh@mox1.hyak.local>
2021-10-20 15:19:40 -0700

    brhod_pp oop

---
+++ oop_s_rOH_opt/brhod_pp/g16.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=brhod_pp
+#SBATCH --nodes=1 
+#SBATCH --time=3:00:00
+#SBATCH --mem=128G
+#SBATCH --chdir=/gscratch/ilahie/mccoygrp/coire_hocl/oop_s_rOH_opt/brhod_pp/
+#SBATCH --partition=ilahie
+#SBATCH --account=ilahie
+
+# load Gaussian environment
+module load contrib/g16.b01
+
+# debugging information
+echo "**** Job Debugging Information ****"
+echo "This job will run on $SLURM_JOB_NODELIST"
+echo ""
+echo "ENVIRONMENT VARIABLES"
+set
+echo "**********************************************" 
+
+# run Gaussian
+g16 brhod_pp_oop_00_sp.gjf
+g16 brhod_pp_oop_05_sp.gjf
+g16 brhod_pp_oop_10_sp.gjf
+g16 brhod_pp_oop_15_sp.gjf
+g16 brhod_pp_oop_20_sp.gjf
+g16 brhod_pp_oop_25_sp.gjf
+g16 brhod_pp_oop_30_sp.gjf
+g16 brhod_pp_oop_35_sp.gjf
+g16 brhod_pp_oop_40_sp.gjf
+g16 brhod_pp_oop_45_sp.gjf
+g16 brhod_pp_oop_50_sp.gjf
+#g16 brhod_pp_oop_oop_s.gjf
+
