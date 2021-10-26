d49aadb7fafea6c63c20340e1400efba135ca889
C. Gavin-Hanner <cgavinh@mox1.hyak.local>
2021-10-26 13:24:50 -0700

    reran ihod rOH without ECP error

--- rOH_s_oop_0/ihod/g16_eq.sh
+++ rOH_s_oop_0/ihod/g16_eq.sh
@@ -20,4 +20,20 @@ echo "**********************************************"
 
 #run Gaussian Jobs
 
-g16 ihod_rOH_eq_sp.gjf
+#g16 ihod_0_opt.gjf
+g16 ihod_rOH_075_sp.gjf
+g16 ihod_rOH_080_sp.gjf
+g16 ihod_rOH_085_sp.gjf
+g16 ihod_rOH_090_sp.gjf
+g16 ihod_rOH_095_sp.gjf
+g16 ihod_rOH_100_sp.gjf
+g16 ihod_rOH_105_sp.gjf
+g16 ihod_rOH_110_sp.gjf
+g16 ihod_rOH_115_sp.gjf
+g16 ihod_rOH_120_sp.gjf
+g16 ihod_rOH_125_sp.gjf
+g16 ihod_rOH_130_sp.gjf
+g16 ihod_rOH_135_sp.gjf
+g16 ihod_rOH_140_sp.gjf
+g16 ihod_rOH_145_sp.gjf
+g16 ihod_rOH_150_sp.gjf
