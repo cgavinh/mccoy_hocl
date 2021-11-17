1873da188ef34cb17d2423fd4d4a9dde2daf1498
C. Gavin-Hanner <cgavinh@mox2.hyak.local>
2021-11-16 16:16:21 -0800

    pot scan for hocl and hod

---
+++ oop_s_rOH_f/brhocl/gen_sp.sh
@@ -0,0 +1,12 @@
+#!bin/bash/
+sed brhocl_oop_00_sp.gjf -e 's/00/05/g' -e 's/OOP= 90./OOP= 85./g' > brhocl_oop_05_sp.gjf
+sed brhocl_oop_00_sp.gjf -e 's/00/10/g' -e 's/OOP= 90./OOP= 80./g' > brhocl_oop_10_sp.gjf
+sed brhocl_oop_00_sp.gjf -e 's/00/15/g' -e 's/OOP= 90./OOP= 75./g' > brhocl_oop_15_sp.gjf
+sed brhocl_oop_00_sp.gjf -e 's/00/20/g' -e 's/OOP= 90./OOP= 70./g' > brhocl_oop_20_sp.gjf
+sed brhocl_oop_00_sp.gjf -e 's/00/25/g' -e 's/OOP= 90./OOP= 65./g' > brhocl_oop_25_sp.gjf
+sed brhocl_oop_00_sp.gjf -e 's/00/30/g' -e 's/OOP= 90./OOP= 60./g' > brhocl_oop_30_sp.gjf
+sed brhocl_oop_00_sp.gjf -e 's/00/35/g' -e 's/OOP= 90./OOP= 55./g' > brhocl_oop_35_sp.gjf
+sed brhocl_oop_00_sp.gjf -e 's/00/40/g' -e 's/OOP= 90./OOP= 50./g' > brhocl_oop_40_sp.gjf
+sed brhocl_oop_00_sp.gjf -e 's/00/45/g' -e 's/OOP= 90./OOP= 45./g' > brhocl_oop_45_sp.gjf
+sed brhocl_oop_00_sp.gjf -e 's/00/50/g' -e 's/OOP= 90./OOP= 40./g' > brhocl_oop_50_sp.gjf
+
