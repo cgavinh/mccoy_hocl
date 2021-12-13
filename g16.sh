e242cee8c6da36bf0ba18a6dba550fce79324d65
C. Gavin-Hanner <cgavinh@mox1.hyak.local>
2021-12-13 11:18:53 -0800

    fully relaxed calculation

---
+++ g16.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=clhocl
+#SBATCH --nodes=1 
+#SBATCH --time=3:00:00
+#SBATCH --mem=128G
+#SBATCH --chdir=/gscratch/ilahie/mccoygrp/coire_hocl/
+#SBATCH --partition=ilahie
+#SBATCH --account=ilahie
+
+# load Gaussian environment
+module load contrib/g16.b01
+
+# debugging information
+echo "**** Job Debugging Information ****"
+echo "This job will run on $SLURM_JOB_NODELIST"
+echo ""
+echo "ENVIRONMENT VARIABLES"
+set
+echo "**********************************************" 
+
+# run Gaussian
+g16 clhocl_fully_relaxed.gjf
