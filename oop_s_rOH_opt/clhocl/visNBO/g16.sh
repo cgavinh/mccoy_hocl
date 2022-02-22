f934ff625bbac5cc38035aa1838f32c87c3c7c5c
C. Gavin-Hanner <cgavinh@mox1.hyak.local>
2022-02-22 12:11:38 -0800

    tried saveNBO keyword

---
+++ oop_s_rOH_opt/clhocl/visNBO/g16.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=clhocl
+#SBATCH --nodes=1 
+#SBATCH --time=3:00:00
+#SBATCH --mem=128G
+#SBATCH --chdir=/gscratch/ilahie/mccoygrp/coire_hocl/oop_s_rOH_opt/clhocl/visNBO/
+#SBATCH --partition=ilahie
+#SBATCH --account=ilahie
+
+# load Gaussian environment
+module load contrib/g16.b01
+
+# debugging information
+echo "**** Job Debugging Information ****"
+echo "This job will run on $SLURM_JOB_NODELIST"
+echo ""
+echo "ENVIRONMENT VARIABLES"
+set
+echo "**********************************************" 
+
+# run Gaussian
+g16 clhocl_0_sp.gjf
+formchk clhocl_0_sp.chk
+
+
+
